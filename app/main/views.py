6ad54045e578a0123271c78bb871d7126026b2a0
Benie <davinci.monalissa@gmail.com>
2022-05-14 00:06:28 +0300

    add an update profile view function.

--- app/main/views.py
+++ app/main/views.py
@@ -46,3 +46,25 @@ def profile(uname):
         abort(404)
 
     return render_template('profile/profile.html',user=user,user_pitches=user_pitches)
+
+
+@main.route('/user/<uname>/update',methods=['GET','POST'])
+@login_required
+def update_profile(uname):
+
+    user = Users.query.filter_by(username=uname).first()
+
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form=form)
